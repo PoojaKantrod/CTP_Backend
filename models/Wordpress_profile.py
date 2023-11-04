f557f98de4ae359314ff00f0b09217c666326991
PoojaKantrod <poojakantror30@gmail.com>
2023-11-04 11:31:19 -0500

    Updated Data_cleaning file, social_media_Icon

---
+++ models/Wordpress_profile.py
@@ -0,0 +1,134 @@
+from database import DBConnection
+class Wordpress_profile:
+    def __init__(self):
+        return
+
+    def get_all_profiles(self):
+        """
+        Get a Wordpress profiles
+        """
+        with DBConnection() as db_connection:
+            cursor = db_connection.cursor(dictionary=True)
+            query = "SELECT * FROM cleaned_data"
+            cursor.execute(query)
+            profiles = cursor.fetchall()
+            if not profiles:
+                return
+            return profiles
+
+    def get_by_id(self, uniqueId):
+        """
+        Get a Wordpress profile by unique id
+        """
+        with DBConnection() as db_connection:
+            cursor = db_connection.cursor(dictionary=True)
+            query = "SELECT * FROM cleaned_data WHERE uniqueId = %s"
+            cursor.execute(query, (uniqueId,))
+            profile = cursor.fetchone()
+            if not profile:
+                return
+            return profile
+
+    def delete_by_id(self, uniqueId):
+        """
+        Delete a Wordpress profile by unique id
+        """
+        with DBConnection() as db_connection:
+            cursor = db_connection.cursor()
+            query = "DELETE FROM cleaned_data WHERE uniqueId = %s"
+            result = cursor.execute(query, (uniqueId,))
+            db_connection.commit()
+            if result.rowcount > 0:
+                return {"message": "Profile deleted successfully", "status_code":200}
+            else:
+                return {"message":f"Failed to delete profile with {uniqueId}", "status_code":200}
+
+    def update_profile_by_id(self, uniqueId, data):
+        """
+        Update profile by the data. UniqueId is the ID used to identify row.
+        """
+        with DBConnection() as db_connection:
+            cursor = db_connection.cursor(dictionary=True)
+
+            # Check if the profile with the given uniqueId exists
+            check_query = "SELECT * FROM cleaned_data WHERE uniqueId = %s"
+            cursor.execute(check_query, (uniqueId,))
+            existing_profile = cursor.fetchone()
+
+            if not existing_profile:
+                return {"message":"Profile not found", "status_code":404}
+
+            # Prepare the UPDATE query dynamically
+            update_query = "UPDATE Wordpress_profile SET "
+            update_values = []
+
+            for key, value in data.items():
+                if key != "uniqueId":
+                    update_query += f"`{key}` = %s, "  # Use backticks to handle special characters
+                    update_values.append(value)
+
+            # Remove the trailing comma and space from the query
+            update_query = update_query.rstrip(', ')
+
+            update_query += " WHERE uniqueId = %s"
+            update_values.append(uniqueId)
+
+            cursor.execute(update_query, update_values)
+            db_connection.commit()
+            return {"message":"Profile updated successfully", "status_code": 200}
+
+    def create_profile(self, data):
+        with DBConnection() as db_connection:
+            cursor = db_connection.cursor(dictionary=True)
+
+            # Insert the new profile data into the database
+            insert_query = """
+                INSERT INTO cleaned_data (
+                    uniqueId, firstName, middleName, lastName, orderNumber, emailUsed, birthDate, bannerPhoto, 
+                    usOrCanadianOrInternationalSchool, cannotFindSchoolPleaseProvideIt, Degree, graduationYear,
+                    usUndergradSchool, aboutMe, myAcademics, myExtracurricularActivities, myAthletics, myPlansForFuture,
+                    myJobsInternships, mySkills, myLanguages, myHonorsAwards, favoriteBook, favoriteQuote, 
+                    favoriteCharitableCauses, regionsAndCharitableNeedsICareAbout, metropolitanAreasWhoseCharitableNeedsICareAbout,
+                    ethnicGroupsWhoseCharitableNeedsICareAbout, religiousGroupsWhoseCharitableNeedsICareAbout,
+                    howMyLifestyleIsMakingtheWorldBetterPlace, `favoriteNonprofitOrganizations`, volunteeringCommunityService,
+                    myFundraisingActivities, charitableWishlists, doYouBelongToGreek, shareYourNationalSocialFraternityMembership,
+                    nameYourNationalSocialFraternityIfNotInList, yourNationalSocialSororityMembership,
+                    nameYourNationalSocialSororityIfNotInList, yourNationalFraternityAssociations,
+                    yourNationalFraternityAssociationIfNotInList, yourNationalOrganizationsMembership,
+                    YourOtherNationalOrganizationWhichYouCannotFind, menSportsTeam, womenSportsTeam,
+                    yourMenSportsTeamIfNotInList, yourWomenSportsTeamIfNotInList, includeTwelveStudentclubsToWhichYouBelong,
+                    yourOwnWebsite, yourBlog, LinkedIn, Instagram, Twitter, Facebook, GooglePlus, Pinterest, Youtube, Flickr, Behance,
+                    Tumblr, Etsy, WayUp, academiaEdu, Researchgate, Digication, Issuu, VSCO, 500px, helperHelper, Github,
+                    projectsThatMatterorg, Quora, TikTok, Strava, sportsRecruits, mileSplit, prestoSports, Harri,
+                    eliteProspects, Hudl, maxPreps, NCSA, athleticNet, medium, twitch, soundCloud, artStation, firstRobotics,
+                    Patreon, soundClick, bandcamp, vexRobotics, Rivals, myThoughtsOnMakingaDifference, swimCloud, placesIHaveLived,
+                    placesIHaveTraveled, myFavoritePodcasts, newUniqueId, id
+                ) VALUES (
+                    %(uniqueId)s, %(firstName)s, %(middleName)s, %(lastName)s, %(orderNumber)s, %(emailUsed)s, %(birthDate)s,
+                    %(bannerPhoto)s, %(usOrCanadianOrInternationalSchool)s, %(cannotFindSchoolPleaseProvideIt)s, %(Degree)s,
+                    %(graduationYear)s, %(usUndergradSchool)s, %(aboutMe)s, %(myAcademics)s, %(myExtracurricularActivities)s,
+                    %(myAthletics)s, %(myPlansForFuture)s, %(myJobsInternships)s, %(mySkills)s, %(myLanguages)s,
+                    %(myHonorsAwards)s, %(favoriteBook)s, %(favoriteQuote)s, %(favoriteCharitableCauses)s,
+                    %(regionsAndCharitableNeedsICareAbout)s, %(metropolitanAreasWhoseCharitableNeedsICareAbout)s,
+                    %(ethnicGroupsWhoseCharitableNeedsICareAbout)s, %(religiousGroupsWhoseCharitableNeedsICareAbout)s,
+                    %(howMyLifestyleIsMakingtheWorldBetterPlace)s, %(favoriteNonprofitOrganizations)s, %(volunteeringCommunityService)s,
+                    %(myFundraisingActivities)s, %(charitableWishlists)s, %(doYouBelongToGreek)s, %(shareYourNationalSocialFraternityMembership)s,
+                    %(nameYourNationalSocialFraternityIfNotInList)s, %(yourNationalSocialSororityMembership)s,
+                    %(nameYourNationalSocialSororityIfNotInList)s, %(yourNationalFraternityAssociations)s,
+                    %(yourNationalFraternityAssociationIfNotInList)s, %(yourNationalOrganizationsMembership)s,
+                    %(YourOtherNationalOrganizationWhichYouCannotFind)s, %(menSportsTeam)s, %(womenSportsTeam)s,
+                    %(yourMenSportsTeamIfNotInList)s, %(yourWomenSportsTeamIfNotInList)s,
+                    %(includeTwelveStudentclubsToWhichYouBelong)s, %(yourOwnWebsite)s, %(yourBlog)s, %(LinkedIn)s, %(Instagram)s,
+                    %(Twitter)s, %(Facebook)s, %(GooglePlus)s, %(Pinterest)s, %(Youtube)s, %(Flickr)s, %(Behance)s, %(Tumblr)s,
+                    %(Etsy)s, %(WayUp)s, %(academiaEdu)s, %(Researchgate)s, %(Digication)s, %(Issuu)s, %(VSCO)s, %(500px)s, %(helperHelper)s,
+                    %(Github)s, %(projectsThatMatterorg)s, %(Quora)s, %(TikTok)s, %(Strava)s, %(sportsRecruits)s, %(mileSplit)s,
+                    %(prestoSports)s, %(Harri)s, %(eliteProspects)s, %(Hudl)s, %(maxPreps)s, %(NCSA)s, %(athleticNet)s, %(medium)s,
+                    %(twitch)s, %(soundCloud)s, %(artStation)s, %(firstRobotics)s, %(Patreon)s, %(soundClick)s, %(bandcamp)s,
+                    %(vexRobotics)s, %(Rivals)s, %(myThoughtsOnMakingaDifference)s, %(swimCloud)s, %(placesIHaveLived)s,
+                    %(placesIHaveTraveled)s, %(myFavoritePodcasts)s, %(newUniqueId)s, %(id)s
+                )
+            """
+            cursor.execute(insert_query, data)
+            db_connection.commit()
+
+        return {"message":"Profile created successfully", "status_code": 201}
---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+import models.Wordpress_profile
